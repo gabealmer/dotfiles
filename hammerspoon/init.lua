ea187b4f397979b7b5caf420ccfeb5f052e333f4
Gabor Almer <galmer@whitepages.com>
2015-12-16 11:46:44 +0100

    add hammerspoon window movement

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,3 +7,74 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", function()
   local win = hs.window.focusedWindow()
   win:maximize()
 end)
+
+hs.hotkey.bind({"cmd"}, "J", function()
+  local app = hs.application.frontmostApplication()
+  if app:name() == "Safari" then
+    hs.eventtap.keyStroke({}, "down")
+  end
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "J", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "K", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "L", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"shift", "cmd", "alt", "ctrl"}, "H", function()
+  local win = hs.window.focusedWindow()
+  local nextScreen = win:screen():previous()
+  win:moveToScreen(nextScreen)
+end)
+
+hs.hotkey.bind({"shift", "cmd", "alt", "ctrl"}, "L", function()
+  local win = hs.window.focusedWindow()
+  local nextScreen = win:screen():next()
+  win:moveToScreen(nextScreen)
+end)
