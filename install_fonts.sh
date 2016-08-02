06577aa8d04b7ef64aa00092d8c379809b5df1dd
Gabor Almer <gabealmer@gmail.com>
2016-08-02 12:37:38 +0200

    install fonts

---
+++ install_fonts.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+curl -o ~/Library/Fonts/Inconsolata\ for\ Powerline.otf https://raw.githubusercontent.com/powerline/fonts/master/Inconsolata/Inconsolata%20for%20Powerline.otf
+
+curl -o ~/Library/Fonts/Roboto-Black.ttf https://raw.githubusercontent.com/google/fonts/master/apache/roboto/Roboto-Black.ttf
+curl -o ~/Library/Fonts/Roboto-Bold.ttf https://raw.githubusercontent.com/google/fonts/master/apache/roboto/Roboto-Bold.ttf
+curl -o ~/Library/Fonts/Roboto-Black.ttf https://raw.githubusercontent.com/google/fonts/master/apache/roboto/Roboto-Black.ttf
+curl -o ~/Library/Fonts/Roboto-Light.ttf https://raw.githubusercontent.com/google/fonts/master/apache/roboto/Roboto-Light.ttf
+curl -o ~/Library/Fonts/Roboto-Medium.ttf https://raw.githubusercontent.com/google/fonts/master/apache/roboto/Roboto-Medium.ttf
+curl -o ~/Library/Fonts/Roboto-Regular.ttf https://raw.githubusercontent.com/google/fonts/master/apache/roboto/Roboto-Regular.ttf
+curl -o ~/Library/Fonts/Roboto-Thin.ttf https://raw.githubusercontent.com/google/fonts/master/apache/roboto/Roboto-Thin.ttf
+
+curl -o ~/Library/Fonts/FontAwesome.otf https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/fonts/FontAwesome.otf
+
+curl -o ~/Library/Fonts/glyphicons-halflings-regular.ttf https://raw.githubusercontent.com/twbs/bootstrap/master/fonts/glyphicons-halflings-regular.ttf
