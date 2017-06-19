830da7178e9545cb24cd83a0d8082a0c621e1030
Gabor Almer <gabealmer@gmail.com>
2017-06-19 11:05:04 +0200

    install fonts

--- install_fonts.sh
+++ install_fonts.sh
@@ -13,3 +13,18 @@ curl -o ~/Library/Fonts/Roboto-Thin.ttf https://raw.githubusercontent.com/google
 curl -o ~/Library/Fonts/FontAwesome.otf https://raw.githubusercontent.com/FortAwesome/Font-Awesome/master/fonts/FontAwesome.otf
 
 curl -o ~/Library/Fonts/glyphicons-halflings-regular.ttf https://raw.githubusercontent.com/twbs/bootstrap/master/fonts/glyphicons-halflings-regular.ttf
+
+curl -o ~/Library/Fonts/SourceCodePro-Black.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-Black.ttf
+curl -o ~/Library/Fonts/SourceCodePro-BlackIt.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-BlackIt.ttf
+curl -o ~/Library/Fonts/SourceCodePro-Bold.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-Bold.ttf
+curl -o ~/Library/Fonts/SourceCodePro-BoldIt.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-BoldIt.ttf
+curl -o ~/Library/Fonts/SourceCodePro-ExtraLight.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-ExtraLight.ttf
+curl -o ~/Library/Fonts/SourceCodePro-ExtraLightIt.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-ExtraLightIt.ttf
+curl -o ~/Library/Fonts/SourceCodePro-It.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-It.ttf
+curl -o ~/Library/Fonts/SourceCodePro-Light.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-Light.ttf
+curl -o ~/Library/Fonts/SourceCodePro-LightIt.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-LightIt.ttf
+curl -o ~/Library/Fonts/SourceCodePro-Medium.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-Medium.ttf
+curl -o ~/Library/Fonts/SourceCodePro-MediumIt.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-MediumIt.ttf
+curl -o ~/Library/Fonts/SourceCodePro-Regular.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-Regular.ttf
+curl -o ~/Library/Fonts/SourceCodePro-Semibold.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-Semibold.ttf
+curl -o ~/Library/Fonts/SourceCodePro-SemiboldIt.ttf https://raw.githubusercontent.com/adobe-fonts/source-code-pro/release/TTF/SourceCodePro-SemiboldIt.ttf
